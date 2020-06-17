71c274ff908531a3957187ec00fd67c1e261e276
unknown <ianangillis@gmail.com>
2020-06-17 12:41:17 +0200

    added randomizer

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 "use strict";
 
-window.onload = letUserKnowItWorks;
+window.onload = attachHandlers;
 
-function letUserKnowItWorks(){
-    console.log("Hello there");
+function attachHandlers(){
+    document.getElementById("dropButton").addEventListener("click", onDropButtonClick);
+    console.log("eventhandlers attached");
+}
+
+//Positions on the warzone map
+let positions =  ["Dam", 
+"Military Base", 
+"Quarry", 
+"Airport", 
+"Storage Town", 
+"Superstore", 
+"Boneyard", 
+"Train Station", 
+"Promenade West", 
+"Promenade East", 
+"Hills",
+"TV Station",
+"Hospital",
+"Stadium",
+"Lumber",
+"Farmland",
+"Downtown",
+"Park",
+"Port",
+"Prison"]; 
+
+function onDropButtonClick(){
+    console.log("dropbutton was clicked");
+    let positionIndex = getRandomArbitrary(0, positions.length);
+    let dropResultParagraph = document.getElementById('dropResult');
+    dropResultParagraph.innerHTML = positions[positionIndex];
+}
+
+function getRandomArbitrary(minValue, Maxvalue){
+    return Math.round(Math.random() * (Maxvalue - minValue) + minValue);
 }
